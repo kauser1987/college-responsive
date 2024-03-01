4dfa2fd43690906d4b921d2268ef8f3c251bda01
kauser1987 <kauser1987@gmail.com>
2024-03-01 11:49:59 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-text">
+        <h1>we ensure better education for better world</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
